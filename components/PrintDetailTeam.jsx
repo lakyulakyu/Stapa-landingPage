076a44d4039949b4c97b735b224ff76278df76ba
lakyulakyu <martieszhidan@gmail.com>
2023-06-01 13:20:58 +0700

    fix:error deployment bug

--- components/PrintDetailTeam.jsx
+++ components/PrintDetailTeam.jsx
@@ -1,11 +1,11 @@
-import htmlToImage from 'html-to-image';
+import htmlToImage from 'html-to-image'
 
 export const printHTMLToPNG = (element) => {
   htmlToImage.toPng(element)
     .then(function (dataUrl) {
       const link = document.createElement('a');
       link.href = dataUrl;
-      link.download = 'output.png';
+      link.download = 'detail_team.png';
       link.click();
     })
     .catch(function (error) {
