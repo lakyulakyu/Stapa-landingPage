55f62bf77aaf097dca9a23253009ac46f7bd4ad3
lakyulakyu <martieszhidan@gmail.com>
2023-07-06 14:33:10 +0700

    add google API

--- app/Video/page.jsx
+++ app/Video/page.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import CardVideos from "@/components/Card/CardVideos";
+// import React from "react";
+// import CardVideos from "@/components/Card/CardVideos";
 
-async function getData() {
-  const res = await fetch(
-    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAdzQyw7F__Nr1IfTMFh-GGhwvhG6wMAG0&channelId=UCeHsWLotqZDJTdl1xJZLk-w&part=snippet&maxResults=10"
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+// async function getData() {
+//   const res = await fetch(
+//     "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAdzQyw7F__Nr1IfTMFh-GGhwvhG6wMAG0&channelId=UCeHsWLotqZDJTdl1xJZLk-w&part=snippet&maxResults=10"
+//   );
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
 
-export default async function page() {
-  const data = await getData();
+// export default async function page() {
+//   const data = await getData();
 
-  console.log(data);
-  const videos = data.items.map((item) => ({
-    title: item.snippet.title,
-    thumbnail: item.snippet.thumbnails.default.url,
-    videoId: item.id.videoId,
-    description: item.snippet.description,
-  }));
+//   console.log(data);
+//   const videos = data.items.map((item) => ({
+//     title: item.snippet.title,
+//     thumbnail: item.snippet.thumbnails.default.url,
+//     videoId: item.id.videoId,
+//     description: item.snippet.description,
+//   }));
 
-  return (
-    <>
-      <div className=" w-full h-fit md:mt-44 mx-auto mt-24 flex flex-wrap   ">
-        {videos.map((video) => (
-          <CardVideos key={video.videoId} video={video} />
-        ))}
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div className=" w-full h-fit md:mt-44 mx-auto mt-24 flex flex-wrap   ">
+//         {videos.map((video) => (
+//           <CardVideos key={video.videoId} video={video} />
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
